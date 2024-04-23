853ceec14df47738f427608d55c526098b25d4b3
jamell <jcolemanjr@bostonk12.org>
2024-04-23 10:43:52 -0400

    intger input assignment '''

---
+++ Python/Week_13/2024-03-07/ practice2.py
@@ -0,0 +1,4 @@
+#jamell c lamont
+#march8th 2024
+name=input("please enter your name:")
+print(F"Hi {name}, I hope you are having an awesome day!")
---
+++ Python/Week_13/week16/practice.py
@@ -0,0 +1,5 @@
+#jamell
+#4/23/2024
+
+animes
+name ["1 jujutsu kaisen"]
