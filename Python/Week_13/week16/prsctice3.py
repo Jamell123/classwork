6f943ddb046284295560d5bd1b896a903476d9de
jamell <jcolemanjr@bostonk12.org>
2024-05-08 09:28:22 -0400

    intger input assignment '''

---
+++ Python/Week_13/week16/prsctice3.py
@@ -0,0 +1,15 @@
+#jamell coleman
+4/25/2024
+
+#this is how you create a empty list
+a=["glazington"]
+
+#this is how you create a list
+b=[1,2,3,4]
+
+print(a)
+print(b)
+
+a = ("glazington")
+
+b =[]
---
+++ Python/week17/2024/05.06/practice.py
@@ -0,0 +1,17 @@
+#jamell coleman
+#5/8/2024
+
+numbers=[1]
+numbers=[2]
+numbers=[3]
+numbers=[4]
+numbers=[5]
+numbers=[6]
+numbers=[7]
+numbers=[8]
+numbers=[9]
+numbers=[10]
+
+
+numbers2=[]
+
