c41a3d5d7d8d150c409329f71a68a862f4e144a7
jamell <jcolemanjr@bostonk12.org>
2024-05-08 10:05:17 -0400

    intger input assignment '''

---
+++ Python/2024/05/08/lists.py
@@ -0,0 +1,12 @@
+#jamell coleman
+# 5/8/2024
+#
+# #creating a empty list
+lst= []
+
+#using a four loop to add the numbers 1/100 to the list
+for py in range(100):
+    lst+= [py]
+
+#printing the list lst
+print(lst)
