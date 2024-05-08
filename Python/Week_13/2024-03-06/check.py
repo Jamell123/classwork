c6e57a48adc8085dbb2016bc8d85aedf866786f7
jamell <jcolemanjr@bostonk12.org>
2024-05-08 09:28:36 -0400

    intger input assignment '''

---
+++ Python/Week_13/2024-03-06/check.py
@@ -0,0 +1,27 @@
+#jamell coleman
+#5/7/2024
+
+#creating a empty list
+a = []
+
+#using the operator to add to the list
+a+= [1]
+a+=[2]
+a+= [3]
+
+#printing the list
+print(a)
+
+#reating a empty list to add to b
+b=[]
+
+#using the function to add to the list b
+b.appened(1)
+b.appened(2)
+b.appene(3)
+
+#printing the list b
+print(b)
+
+#positional comparison
+print=(a 1s b)
