1bbc86116886b2cde77f3c7015d8ce4258abbf01
jamell123 <jcolemanjr@bostonk12.org>
2025-03-06 13:53:31 -0500

    commit practice

---
+++ 2024-2025/plants2.py
@@ -0,0 +1,8 @@
+def main():
+    number1 =int(input("Please enter a number: "))
+    number2 =int(input("Please enter another number: "))
+
+    print(f"{number1} * {number2} = {number1 * number2}")
+if __name__ == '__main__':
+    main()
+
