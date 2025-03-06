a0fab7c5e91457df46aabf6907685a4c27f9f60b
jamell123 <jcolemanjr@bostonk12.org>
2025-03-06 13:53:50 -0500

    commit practice

--- 2023-2024/Python/[ractice programming python.py
+++ 2023-2024/Python/[ractice programming python.py
@@ -1,3 +1,6 @@
 print("Please enter a number between 1 and 10: 2")
+15
+print("Please enter a number
+print("Please enter a number between 1 and 10: 2")
     15
 print("Please enter a number between 1 and 10: 3")
---
+++ 2024-2025/plant.py
@@ -0,0 +1,9 @@
+from email.contentmanager import maintype
+
+
+def main():
+    fighter=input("who is the est blow for blow fighter?")
+    print(fighter,"is the best blow for blow fighter")
+
+if __name__ == '__main__':
+    main()
