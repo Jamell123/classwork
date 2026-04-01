c1be2e71206fddccbcdb686ac8ed403e70afae40
359328 <jcolemanjr@bostonk12.org>
2026-04-01 07:44:07 -0400

    commit

---
+++ 2026-03-12/io.py
@@ -0,0 +1,11 @@
+#jamell coleman
+#03-12-26
+#file input and output
+
+def main():
+    file = open('wrds.txt','r')
+    info = file.read()
+    1print(info)
+
+if __name__ == '__main__':
+    main()
---
+++ 2026-03-12/minro.io.py
@@ -0,0 +1,3 @@
+input=('please enter your first nsme=')
+int-input('please enter your age=')
+input=()
